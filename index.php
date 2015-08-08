22c02a8c546252da4ace1630e47efda881ed9b68
Evgeniy Pekur <depekur@gmail.com>
2015-08-09 00:43:52 +0300

    hello mr Landos

---
+++ index.php
@@ -0,0 +1,311 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Банановый лендинг</title>
+
+
+
+	<link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+	
+
+<header class="s001">
+	<div class="wrapp">
+		<div class="header-logo">
+			<img src="media/123.jpg">
+		</div>
+		<div class="header-contact">
+			<ul>
+				<li><h3>закажите прямо сейчас!</h3></li>
+				<li>+380958351650</li>
+				<li>+380958351650</li>
+			</ul>
+		</div>
+	</div>
+</header>
+
+<section class="s002">
+	<div class="wrapp">
+		<h1>Главный, сочный, самый вкусный заголовок h1</h1>
+	</div>
+</section>
+
+
+
+<section class="s003">
+	<div class="wrapp">
+	<h2>Закажите сейчас и получите что-то ценное!</h2>
+		<div class="gor-form">
+			<form class="">
+				<input type="text">
+				<input type="text">
+				<button>заказать</button>
+			</form>
+		</div>			
+	</div>
+</section>
+
+<section class="s004">
+<div class="wrapp">
+	<h2>супер продающий заголовок</h2>
+
+
+	<ul>
+		<li>
+
+			<h3>заголовок</h3>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+		</li>
+		<li>
+
+			<h3>заголовок</h3>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+		</li>
+		<li>
+
+			<h3>заголовок</h3>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+		</li>
+		<li>
+
+			<h3>заголовок</h3>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+		</li>
+		<li>
+
+			<h3>заголовок</h3>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+		</li>
+		<li>
+
+			<h3>заголовок</h3>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+		</li>
+	</ul>
+
+</div>
+</section>
+
+<section class="s005">		
+<div class="wrapp">
+	<div class="s005-left">
+		<h2>да купи уже наконец-то!</h2>
+		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+		tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+		quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+		consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+		cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+		proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+	</div>
+	
+	<div class="ver-form">
+		<form>
+			<h3>Напишите!</h3>
+			<input type="text">
+			<input type="text">
+			<button>заказать!</button>
+		</form>
+	</div>
+</div>
+</section>
+
+<section class="s006">
+<div class="wrapp">
+	<h2>Если ты сейчас не купишь - я напишу еще один заголовок!</h2>
+	<ul>
+		<li>
+
+			<h3>header</h3>				
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+			tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+			quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+			consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+			cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+			proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+		</li>
+		<li>
+
+			<h3>header</h3> 				
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+			tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+			quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+			consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+			cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+			proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+		</li>
+		<li>
+
+			<h3>header</h3> 
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+			tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+			quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+			consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+			cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+			proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+		</li>
+	</ul>
+</div>
+</section>
+
+<section class="s007">
+<div class="wrapp">
+	<h2>Всему продающему есть предел (только не заголовкам)</h2>
+	<ul>
+		<li>
+			<img src="media/123.jpg">
+			<h3>header</h3>				
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+			tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+			quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+			consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+			cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+			proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+		</li>
+		<li>
+			<img src="media/123.jpg">
+			<h3>header</h3> 				
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+			tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+			quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+			consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+			cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+			proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+		</li>
+		<li>
+			<img src="media/123.jpg">
+			<h3>header</h3> 
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+			tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+			quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+			consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+			cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+			proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+		</li>
+	</ul>
+</div>	
+</section>
+
+<section class="s008">
+<div class="wrapp">
+	<h2>Ну все, или ты покупаешь, или я вызываю копов!</h2>
+	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+	tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+	quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+	consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+	cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+	proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+	tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+	quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+	consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+	cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+	proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+</div>
+</section>
+
+<section class="s009">
+<div class="wrapp">
+<h2>Кажется ты заинтересовался. Ха-ха! Еще немного ты купишь</h2>
+	<ol>
+	<div class="s009-left">
+		<li>Lorem ipsum dolor sit amet, consectetur adipisicing elit</li>
+		<li>Lorem ipsum dolor sit amet, consectetur adipisicing elit</li>
+		<li>Lorem ipsum dolor sit amet, consectetur adipisicing elit</li>
+		<li>Lorem ipsum dolor sit amet, consectetur adipisicing elit</li>
+		<li>Lorem ipsum dolor sit amet, consectetur adipisicing elit</li>
+		<li>Lorem ipsum dolor sit amet, consectetur adipisicing elit</li>
+		<li>Lorem ipsum dolor sit amet, consectetur adipisicing elit</li>
+		<li>Lorem ipsum dolor sit amet, consectetur adipisicing elit</li>
+	</div>
+	<div class="s009-right">
+		<li>Lorem ipsum dolor sit amet, consectetur adipisicing elit</li>
+		<li>Lorem ipsum dolor sit amet, consectetur adipisicing elit</li>
+		<li>Lorem ipsum dolor sit amet, consectetur adipisicing elit</li>
+		<li>Lorem ipsum dolor sit amet, consectetur adipisicing elit</li>
+		<li>Lorem ipsum dolor sit amet, consectetur adipisicing elit</li>
+		<li>Lorem ipsum dolor sit amet, consectetur adipisicing elit</li>
+		<li>Lorem ipsum dolor sit amet, consectetur adipisicing elit</li>
+	</div>
+		
+	</ol>
+</div>
+</section>
+
+<section class="s010">
+<div class="wrapp">
+	<div class="s010-left">
+		<h2>заголовок</h2>
+		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+		tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+		quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+		consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+		cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+		proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+		<button>а ну быстро нажал!</button>
+	</div>
+	<div class="s010-right">
+		<img src="media/123.jpg">
+	</div>
+</div>
+</section>
+
+
+<section class="s011">
+<div class="wrapp">
+	<h2>Как на счет еще одного продающего заголовка?</h2>
+	<ul>
+		<li>
+			<h3>header</h3>				
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+			tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+			quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+			consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+			cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+			proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+			<button>нажми меня</button>
+		</li>
+		<li>
+
+			<h3>header</h3> 				
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+			tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+			quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+			consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+			cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+			proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+			<button>нажми меня</button>
+		</li>
+		<li>
+
+			<h3>header</h3> 
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+			tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+			quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+			consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+			cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+			proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+			<button>нажми меня</button>
+		</li>
+	</ul>
+</div>
+</section>
+
+
+<section class="s012">
+<div class="wrapp">
+	<div class="s012-left">
+		<h2>Гарантия 146%, вы не поверите, но так на самом деле бывает!</h2>
+		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+		tempor incididunt ut labore et dolore magna aliqua. </p>
+	</div>
+	<div class="s012-right">
+		<img src="media/123.jpg">
+	</div>
+</div>
+</section>
+
+</body>
+</html>
