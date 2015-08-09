b04852c63bfbd590608d333940b835ae98447f0f
Evgeniy Pekur <depekur@gmail.com>
2015-08-10 01:27:39 +0300

    some change

---
+++ js/app.js
@@ -0,0 +1,7 @@
+$('.slider').lbSlider({
+	 leftBtn: '#arrow-left', // left control selector
+	 rightBtn: '#arrow-right', // right control selector
+	 visible: 1, // visible elements quantity
+	 autoPlay: true, // autoscroll flag (default: false)
+	 autoPlayDelay: 1 // delay of autoscroll in seconds (default: 10)
+});
